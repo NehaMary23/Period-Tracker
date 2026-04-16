f7658e53cd27168c39ba3237545841c24632f025
NehaMary23 <nehamaryp@gmail.com>
2026-04-16 19:43:22 +0530

    deploying

--- period_tracker/settings.py
+++ period_tracker/settings.py
@@ -1,7 +1,7 @@
 """
 Django settings for period_tracker project.
 """
-
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -61,12 +61,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'period_tracker.wsgi.application'
 
-# Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
 
 # Password validation
